339cf681a971871d23fc28b875c1d85450f89ca8
Elodie Collier <egcollier19@gmail.com>
2024-10-03 22:40:09 -0400

    Initial Commit

---
+++ whiskit/whiskitApp.swift
@@ -0,0 +1,17 @@
+//
+//  whiskitApp.swift
+//  whiskit
+//
+//  Created by Elodie Collier on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct whiskitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
